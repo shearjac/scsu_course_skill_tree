f59c4904d9c31c895e53dea771dd3da5d8afd5fe
Jacob Shearer <MutantBadger@gmail.com>
2021-05-01 14:01:25 -0500

    Rewrote paint function to lay out courses first, draw lines, and then draw the course shapes with names.

--- lib/canvas_page.dart
+++ lib/canvas_page.dart
@@ -24,15 +24,28 @@ class CanvasPage extends StatelessWidget {
   List<Course> buildDemoCourses() {
     List<Course> workingList = [];
 
-    workingList.add(new Course("test", "AHS", "160"));
-    workingList.add(new Course("test", "AHS", "260"));
-    workingList.add(new Course("test", "AHS", "230"));
-    workingList.add(new Course("test", "MATH", "221"));
-    workingList.add(new Course("test", "MATH", "222"));
-    workingList.add(new Course("test", "MATH", "320"));
-    workingList.add(new Course("test", "PHYS", "346"));
-    workingList.add(new Course("test", "PHYS", "234"));
-    workingList.add(new Course("test", "PHYS", "237"));
+    workingList.add(new Course("test", "AHS", "160"));  //0
+    workingList.add(new Course("test", "AHS", "260"));  //1
+    workingList.add(new Course("test", "MATH", "221")); //2
+    workingList.add(new Course("test", "MATH", "222")
+      ..addPrerequisite(workingList[2]));               //3
+    workingList.add(new Course("test", "MATH", "112")); //4
+    workingList.add(new Course("test", "MATH", "113")); //5
+    workingList.add(new Course("test", "MATH", "115")); //6
+    workingList.add(new Course("test", "AHS", "230")
+      ..addPrerequisite(workingList[2])
+      ..addPrerequisite(workingList[3])
+      ..addPrerequisite(workingList[4])
+      ..addPrerequisite(workingList[5])
+      ..addPrerequisite(workingList[6]));               //7
+    workingList.add(new Course("test", "MATH", "320")); //8
+    workingList.add(new Course("test", "PHYS", "234")
+      ..addPrerequisite(workingList[6]));               //9
+    workingList.add(new Course("test", "PHYS", "235")
+      ..addPrerequisite(workingList[9]));               //10
+    workingList.add(new Course("test", "PHYS", "346")
+      ..addPrerequisite(workingList[10]));              //11
+    workingList.add(new Course("test", "PHYS", "237")); //12
 
     return workingList;
   }
